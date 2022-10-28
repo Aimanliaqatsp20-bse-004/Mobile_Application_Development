2657dd427250fc1c808ced638f03127cfd9c4532
Aiman Liaqat <114793732+Aimanliaqatsp20-bse-004@users.noreply.github.com>
2022-10-28 08:24:10 -0700

    Add files via upload

---
+++ CHESSBOARD(TASK).js
@@ -0,0 +1,48 @@
+import { View, StyleSheet, Image } from "react-native";
+function ChessBoard(props) {
+  const blocks = [];
+  for (let index = 1; index <= 8; index++) {
+    for (let yindex = 1; yindex <= 8; yindex++) {
+      blocks.push(
+        <View
+          style={
+            index % 2 == 0
+              ? yindex % 2 == 0
+                ? styles.gblock
+                : styles.bblock
+              : yindex % 2 == 0
+              ? styles.bblock
+              : styles.gblock
+          }
+        ></View>
+      );
+    }
+  }
+  return <View style={styles.board}>{blocks}</View>;
+}
+const styles = StyleSheet.create({
+  board: {
+    justifyContent:"center",
+    width: 420,
+    height: 420,
+    borderColor: "brown",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    borderBottomColor:"grey",
+    borderRightColor:"grey",
+    borderLeftColor:"grey",
+    borderTopColor:"grey",
+  },
+  gblock: {
+    width: 50,
+    height: 50,
+    backgroundColor: "White",
+    borderColor:"gray",
+  },
+  bblock: {
+    width: 50,
+    height: 50,
+    backgroundColor: "black",
+  },
+});
+export default ChessBoard;
